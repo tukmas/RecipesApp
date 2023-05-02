026e253955703569a194729fe3f49cccc5ee9022
Сергей Николаевич <tukmachev.18@yandex.ru>
2023-05-02 14:47:16 +0300

    Сделано ДЗ

---
+++ src/main/java/ru/live/recipesapp/controllers/FirstController.java
@@ -0,0 +1,17 @@
+package ru.live.recipesapp.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+
+    @GetMapping
+    public String Start () {
+        return "Приложение запущено";
+    }
+    @GetMapping("/info")
+    public String Info () {
+        return "Тукмачев Сергей " + " Recipes " + " 02.05.2023г " + " К проекту подключен Spring Framework и сборщик Maven, проект написан на языке программирования Java";
+    }
+}
