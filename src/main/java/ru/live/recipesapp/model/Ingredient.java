bcb629cb991f7b5d89f108a976785b40b6e1b7bf
Сергей Николаевич <tukmachev.18@yandex.ru>
2023-05-14 21:40:47 +0300

    Сделано ДЗ

--- src/main/java/ru/live/recipesapp/model/Ingredient.java
+++ src/main/java/ru/live/recipesapp/model/Ingredient.java
@@ -2,9 +2,11 @@ package ru.live.recipesapp.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Ingredient {
     private String name;
     private int quantityOfIngredients;
